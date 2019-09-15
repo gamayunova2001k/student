fe3cfc6850decb5b30e2f22fbfe2ff9a5df34d49
Kseniya <55260753+gamayunova2001k@users.noreply.github.com>
2019-09-16 00:42:08 +0300

    Add files via upload

---
+++ lab(1.2).cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+#include <string> 
+
+//Написать программу, которая во вводимом с клавиатуры тексте посчитает количество слов в предложениях и выведет результат на экран.
+
+using namespace std;
+
+int main() {
+
+	string str;
+
+	cout << "Enter text: \n";
+	getline(cin, str);
+
+	int m = 0;
+	char c = ' ';
+
+	for (int j = 0; j < str.size(); c = str[j++])
+		if (str[j] != ' ' and c == ' ')
+			m++;
+
+	cout << "The number of words in the text: " << m << endl;
+	return 0;
+}
