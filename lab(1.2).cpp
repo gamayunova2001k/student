027202de01b951e7becf8eaff6ff2f9986d5c4b6
Kseniya <55260753+gamayunova2001k@users.noreply.github.com>
2019-09-17 00:34:49 +0300

    Add files via upload

---
+++ lab(1.2).cpp
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+//Написать программу, которая во вводимом с клавиатуры тексте выберет слова, начинающиеся с гласной буквы, и выведет их на экран.
+
+using namespace std;
+
+int main() {
+
+	string str, word;
+	char vowel[10] = {'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'};
+
+	cout << "Enter text: \n";
+	getline(cin, str);
+	istringstream ist(str);
+
+	cout << "Matching words: \n";
+
+	while (ist >> word)
+		for (int i = 0; i < 10; i++) {
+			if (*word.begin() == vowel[i])
+				cout << word << endl;
+		}
+	return 0;
+}
