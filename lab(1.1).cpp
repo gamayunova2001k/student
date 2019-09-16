2dc6e96b3958deca01262413d6a6f1178f8c5204
Kseniya <55260753+gamayunova2001k@users.noreply.github.com>
2019-09-17 00:18:40 +0300

    Update lab(1.1).cpp

--- lab(1.1).cpp
+++ lab(1.1).cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <iostream>
+#include <sstream>
 #include <string> 
 
 //Написать программу, которая во вводимом с клавиатуры тексте заменит все окончания слов «x» на «z» и выведет результат на экран.
@@ -24,3 +25,27 @@ int main() {
 	cout << "Result: \n" << s0;
 	return 0;
 }
+
+/*
+
+int main() {
+
+	string str, word;
+	char a[2] = {'x','z'};
+
+	cout << "Enter text: \n";
+	getline(cin, str);
+	istringstream ist(str);
+
+	while (ist >> word)
+		for (int i = 0; i < 1; i++) {
+			if (*word.rbegin() == a[0]) {
+				*word.rbegin() = a[1];
+				cout << word << " ";
+			}
+			else cout << word << " ";
+		}
+	return 0;
+}
+
+*/
