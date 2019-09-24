3488132618819d32411a134bc03bf07db139fde9
Kseniya <55260753+gamayunova2001k@users.noreply.github.com>
2019-09-24 19:51:59 +0300

    Add files via upload

---
+++ lab(2.9).cpp
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	int size;
+
+	cout << "Enter size of array: \n";
+	cin >> size;
+
+	int* numbers = new int[size];
+
+	cout << "Enter array elements: \n";
+
+	for (int i = 0; i < size; i++) {
+		cin >> numbers[i];
+	}
+
+	for (int i = 0; i < size; i++)
+		for (int j = 2; j < numbers[i]; j++)
+		{
+			if (numbers[i] % j == 0)
+				break;
+			else if (numbers[i] == j + 1)
+				cout << numbers[i] << " ";
+		}
+	for (int i = 0; i < size; i++)
+		if ((numbers[i] == 1) || (numbers[i] == 2))
+			cout << numbers[i] << " ";
+
+	delete[] numbers;
+	return 0;
+}
