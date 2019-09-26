9bd0b445c31b7a9dcc65f57582237fb8514f8ab1
Kseniya <55260753+gamayunova2001k@users.noreply.github.com>
2019-09-26 23:48:46 +0300

    Update lab(2.9).cpp

--- lab(2.9).cpp
+++ lab(2.9).cpp
@@ -1,7 +1,44 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <Windows.h>
 #include <iostream>
-#include <string>
+
+using namespace std;
+
+int main() {
+
+	int size;
+
+	cout << "Enter size of array: \n";
+	cin >> size;
+
+	int* numbers = new int[size];
+
+	cout << "Enter array elements: \n";
+
+	for (int i = 0; i < size; i++) {
+		cin >> numbers[i];
+	}
+
+	for (int i = 0; i < size; i++) {
+
+		int flag = 1;
+		for (int j = 2; j * j <= numbers[i]; j++)
+			if (numbers[i] % j == 0) {
+				flag = 0;
+				break;
+			}
+		if (flag == 1)
+			cout << numbers[i] << " ";
+
+	}
+	delete[] numbers;
+	return 0;
+}
+
+/*
+#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
 
 using namespace std;
 
@@ -35,3 +72,4 @@ int main() {
 	delete[] numbers;
 	return 0;
 }
+*/
