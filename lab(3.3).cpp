bdd6d5bf7f7c6683db8ee7bf32335c2c015594da
Kseniya <55260753+gamayunova2001k@users.noreply.github.com>
2019-09-30 18:19:38 +0300

    Update lab(3.3).cpp

--- lab(3.3).cpp
+++ lab(3.3).cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
 
 	string str, word;
-	char vowel[42] = {'Q','W','R','T','Y','P','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M','q','w','r','t','y','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'};
+	char vowel[21] = {'Q','W','R','T','Y','P','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
 
 	cout << "Enter text: \n";
 	getline(cin, str);
@@ -18,8 +18,8 @@ int main() {
 	cout << "Matching words: \n";
 
 	while (ist >> word)
-		for (int i = 0; i < 42; i++) {
-			if (*word.rbegin() == vowel[i])
+		for (int i = 0; i < 21; i++) {
+			if (toupper(*word.rbegin()) == vowel[i])
 				cout << word << endl;
 		}
 	return 0;
