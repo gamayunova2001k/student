a08bdebaa34f56ae21cb6998790d12ba670051aa
Kseniya <55260753+gamayunova2001k@users.noreply.github.com>
2019-09-30 16:44:30 +0300

    Update lab(2.9).cpp

--- lab/lab(2.9).cpp
+++ lab/lab(2.9).cpp
@@ -1,6 +1,8 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <Windows.h>
 #include <iostream>
+
+//Написать программу, которая выводит на экран только простые числа. Массив и его длина вводятся пользователем.
 
 using namespace std;
 
@@ -9,26 +11,31 @@ int main() {
 	int size;
 
 	cout << "Enter size of array: \n";
-	cin >> size;
+	cin >> size;	//Вводим размер массива 
 
 	int* numbers = new int[size];
 
 	cout << "Enter array elements: \n";
 
 	for (int i = 0; i < size; i++) {
-		cin >> numbers[i];
+		cin >> numbers[i];	//Вводим элементы массива 
 	}
 
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < size; i++) {	//пробегаемся по каждому элементу массива
+		if (numbers[i] > 1) {	//отбираем элементы, начинающиеся с 2, т.к. отрицательные числа, 0 и 1 не являются простыми числами
+			int flag = 1;
 
-		int flag = 1;
-		for (int j = 2; j * j <= numbers[i]; j++)
-			if (numbers[i] % j == 0) {
-				flag = 0;
-				break;
+			for (int j = 2; j * j <= numbers[i]; j++) {
+				if (numbers[i] % j == 0) {
+					flag = 0;
+					break;
+				}
 			}
-		if (flag == 1)
-			cout << numbers[i] << " ";
+
+			if (flag == 1) {
+				cout << numbers[i] << " ";
+			}
+		}
 
 	}
 	delete[] numbers;
