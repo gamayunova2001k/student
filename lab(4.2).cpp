98be29508292ecbba81fa1744ea6e5359654df17
Kseniya <55260753+gamayunova2001k@users.noreply.github.com>
2019-09-30 18:06:16 +0300

    Add files via upload

---
+++ lab(4.2).cpp
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+
+//Написать программу, которая в тексте файла выберет слова, начинающиеся с гласной буквы и запишет результат в новый файл. 
+//Входные данные не ограничиваются одной строкой.
+
+using namespace std;
+
+void writeToFile() {
+	string str, word;
+	char vowel[10] = { 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u' };
+
+	ifstream file("file.txt");
+	ofstream outFile("outFile.txt");
+	while (getline(file, str)) {
+
+		istringstream ist(str);
+		while (ist >> word) {
+			for (int i = 0; i < 10; i++) {
+				if (*word.begin() == vowel[i])
+					outFile << word << endl;
+			}
+		}
+	}
+}
+
+void outputFile() {
+
+	const int arrayLength = 500;
+	char text[arrayLength];
+
+	ifstream outFile("outFile.txt");
+	if (!outFile.is_open()) {
+		cout << "Error! File not found!";
+	}
+	else {
+		while (outFile.getline(text, arrayLength)) {
+			cout << text << endl;
+		}
+		outFile.close();
+	}
+}
+
+int main() {
+
+	writeToFile();
+
+	outputFile();
+
+	return 0;
+}
