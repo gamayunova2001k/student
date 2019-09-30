568e6f5482db152e37e1f4e332f57c374862f77d
Kseniya <55260753+gamayunova2001k@users.noreply.github.com>
2019-09-30 17:35:55 +0300

    Add files via upload

---
+++ lab/lab(4.1).cpp
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+
+//Написать программу, которая в тексте файла заменит все окончания слов «x» на «z» и запишет результат в новый файл. 
+//Входные данные не ограничиваются одной строкой.
+
+using namespace std;
+
+void writeToFile() {
+
+	const int arrayLength = 500;
+	char text[arrayLength];
+	string str, word;
+
+	ifstream file("file.txt");
+	ofstream outFile("outFile.txt");
+	while (getline(file, str)) {
+
+		istringstream ist(str);
+		while (ist >> word) {
+			for (int i = 0; i < 1; i++) {
+				if (*word.rbegin() == 'x') {
+					*word.rbegin() = 'z';
+					outFile << word << " ";
+				}
+				else {
+					outFile << word << " ";
+				}
+			}
+		}
+
+		outFile << "\n";	
+	}
+}
+
+void outputFile() {
+
+	const int arrayLength = 500;
+	char text[arrayLength];
+
+	ifstream outFile("outFile.txt");
+	if (!outFile.is_open()) {
+		cout << "Error! File not found!";
+	}
+	else {
+		while (outFile.getline(text, arrayLength)) {
+			cout << text << endl;
+		}
+		outFile.close();
+	}
+}
+
+int main() {
+
+	writeToFile();
+
+	outputFile();
+
+	return 0;
+}
